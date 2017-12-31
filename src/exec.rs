2eda24e70ef927198f65e057796f0713f17ecdd1
Ashe Connor <ashe@kivikakk.ee>
2017-12-31 14:35:12 +1100

    add str

--- src/exec.rs
+++ src/exec.rs
@@ -1,28 +1,42 @@
 use display::{flush, format_integer, getline, putch, putstr};
 
 #[cfg(target_arch = "avr")]
+const VHEAP_BASE: u16 = 0x200;
+const VHEAP_SIZE: u16 = 0x200;
+const SHEAP_SIZE: u16 = 0x200;
+
+#[cfg(target_arch = "avr")]
 mod heaps {
     use synced::Synced;
+    use super::{VHEAP_BASE, VHEAP_SIZE};
 
     pub static VHEAP: Synced<Synced<*mut u8>> =
-        unsafe { Synced::new(Synced::new(0x400 as *mut _)) };
-    //    pub static SHEAP: Synced<Synced<*mut u8>> =
-    //        unsafe { Synced::new(Synced::new(0x600 as *mut _)) };
+        unsafe { Synced::new(Synced::new((VHEAP_BASE) as *mut _)) };
+    pub static SHEAP: Synced<Synced<*mut u8>> =
+        unsafe { Synced::new(Synced::new((VHEAP_BASE + VHEAP_SIZE) as *mut _)) };
 }
 
 #[cfg(not(target_arch = "avr"))]
 mod heaps {
     use synced::Synced;
+    use super::{SHEAP_SIZE, VHEAP_SIZE};
 
-    static mut _VHEAP: [u8; 0x200] = [0; 0x200];
+    static mut _VHEAP: [u8; VHEAP_SIZE as usize] = [0; VHEAP_SIZE as usize];
     lazy_static! {
         pub static ref VHEAP: Synced<*mut u8> = unsafe {
             Synced::new(&mut _VHEAP as *const _ as *mut _)
         };
     }
+
+    static mut _SHEAP: [u8; SHEAP_SIZE as usize] = [0; SHEAP_SIZE as usize];
+    lazy_static! {
+        pub static ref SHEAP: Synced<*mut u8> = unsafe {
+            Synced::new(&mut _SHEAP as *const _ as *mut _)
+        };
+    }
 }
 
-use self::heaps::VHEAP;
+use self::heaps::{SHEAP, VHEAP};
 
 #[derive(Debug, PartialEq)]
 pub enum VarValue {
@@ -39,8 +53,6 @@ pub fn run() {
 
     putstr(b"AVR-BASIC\n");
     flush();
-
-    add_var([b'A', 0], &VarValue::Integer(105));
 
     loop {
         let s = getline();
@@ -69,7 +81,8 @@ fn init() {
         use arduino::{ADPS0, ADPS1, REFS0, ADCSRA, ADEN, ADMUX};
         use display;
 
-        VHEAP.write_bytes(0, 0x200);
+        VHEAP.write_bytes(0, VHEAP_SIZE as usize);
+        SHEAP.write_bytes(0, SHEAP_SIZE as usize);
 
         ADMUX.write_volatile(ADMUX.read_volatile() | REFS0);
         ADCSRA.write_volatile(ADCSRA.read_volatile() | ADPS1 | ADPS0);
@@ -89,13 +102,13 @@ fn add_var(vn: [u8; 2], val: &VarValue) {
                     || h.add(2).read() != match *val {
                         VarValue::Integer(..) => 1,
                         VarValue::String(..) => 4,
-                        _ => panic!("eh"),
+                        _ => unreachable!(),
                     }) {
                 let t = h.add(2).read();
                 match t {
                     1 => h = h.add(5),
                     4 => h = h.add(6),
-                    _ => panic!("lol?"),
+                    _ => unreachable!(),
                 }
                 continue;
             }
@@ -114,7 +127,7 @@ fn add_var(vn: [u8; 2], val: &VarValue) {
                     h.add(4).write(o as u8);
                     h.add(5).write(((o >> 8) & 0xFF) as u8);
                 }
-                _ => panic!("ha?"),
+                _ => unreachable!(),
             }
 
             break;
@@ -145,7 +158,7 @@ fn get_var(vn: [u8; 2], t: u8) -> VarValue {
                             let a = h.add(5).read();
                             return VarValue::String(off, ((u16::from(a) << 8) | u16::from(b)));
                         },
-                        _ => panic!("???"),
+                        _ => unreachable!(),
                     }
                 }
             }
@@ -153,7 +166,7 @@ fn get_var(vn: [u8; 2], t: u8) -> VarValue {
             match h.add(2).read() {
                 1 => h = h.add(5),
                 4 => h = h.add(6),
-                _ => panic!("eh"),
+                _ => unreachable!(),
             }
         }
 
@@ -162,8 +175,49 @@ fn get_var(vn: [u8; 2], t: u8) -> VarValue {
             b'!' => VarValue::Single(0.0),
             b'#' => VarValue::Double(0.0),
             b'$' => VarValue::String(0, 0),
-            _ => panic!("gdi"),
+            _ => unreachable!(),
         }
+    }
+}
+
+fn add_str(s: &[u8]) -> (u8, u16) {
+    assert!(s.len() <= 255);
+    let mut so: u16 = 0;
+
+    unsafe {
+        let mut h = **VHEAP;
+
+        loop {
+            if h.read() == 0 {
+                break;
+            }
+
+            match h.add(2).read() {
+                1 => h = h.add(5),
+                4 => {
+                    let off = h.add(3).read();
+                    let b = h.add(4).read();
+                    let a = h.add(5).read();
+                    let len = (u16::from(a) << 8) | u16::from(b);
+                    let latest: u16 = u16::from(off) + len;
+                    if latest > so {
+                        so = latest as u16;
+                    }
+                    h = h.add(6);
+                }
+                _ => unreachable!(),
+            }
+        }
+
+        if so as usize + s.len() >= SHEAP_SIZE as usize {
+            panic!("string heap too large");
+        }
+
+        SHEAP
+            .add(so as usize)
+            .copy_from_nonoverlapping(s as *const [u8] as *const u8, s.len());
+
+        (s.len() as u8, so)
     }
 }
 
@@ -177,12 +231,18 @@ mod tests {
         assert_eq!(get_var([b'A', 0], b'%'), VarValue::Integer(105));
         assert_eq!(get_var([b'A', 0], b'$'), VarValue::String(0, 0));
 
-        add_var([b'A', 0], &VarValue::String(1, 1));
+        let (len, o) = add_str(b"HELLO");
+        assert_eq!(o, 0);
+        assert_eq!(len, 5);
+        add_var([b'A', 0], &VarValue::String(len, o));
         assert_eq!(get_var([b'A', 0], b'%'), VarValue::Integer(105));
-        assert_eq!(get_var([b'A', 0], b'$'), VarValue::String(1, 1));
+        assert_eq!(get_var([b'A', 0], b'$'), VarValue::String(len, o));
 
-        add_var([b'A', 0], &VarValue::String(2, 2));
+        let (len, o) = add_str(b"HI");
+        assert_eq!(o, 5);
+        assert_eq!(len, 2);
+        add_var([b'A', 0], &VarValue::String(len, o));
         assert_eq!(get_var([b'A', 0], b'%'), VarValue::Integer(105));
-        assert_eq!(get_var([b'A', 0], b'$'), VarValue::String(2, 2));
+        assert_eq!(get_var([b'A', 0], b'$'), VarValue::String(len, o));
     }
 }
